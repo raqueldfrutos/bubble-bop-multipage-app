74afa34542747ae128ee4f92862712f29e55919e
wonk93 <danielsalasrodriguez93@gmail.com>
2023-05-05 18:54:10 +0200

    rutas

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,20 +1,89 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcryptjs')
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
 
-const User = require('./../models/User.model')
-
+const User = require("./../models/User.model");
+const { isLoggedOut, isLoggedIn } = require("../middlewares/route-guard");
 
 //Sign up
 
-router.get("/sign-up", (req, res, next) => {
-    res.render ('auth/signup')
-})
-
-router.post("/sign-up", (req, res, next) => {
-
-
+router.get("/sign-up", isLoggedOut, (req, res, next) => {
+  res.render("auth/signup");
 });
 
+/*router.post("/sign-up", (req, res, next) => {
+
+
+});*/
+
+router.post("/sign-up", async (req, res, next) => {
+  try {
+    const { username, password, email } = req.body;
+
+    if (!username || !email || !password) {
+      res.render("auth/signup", {
+        errorMessage: "Por favor rellena todos los campos.",
+      });
+      return;
+    }
+
+    const user = await User.findOne({ $or: [{ username }, { email }] });
+    if (user) {
+      res.render("auth/signup", {
+        errorMessage: "El usuario y/o el email están en uso.",
+      });
+      return;
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    await User.create({ username, email, password: hashedPassword });
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/login", isLoggedOut, (req, res) => {
+  res.render("auth/login");
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.render("auth/login", {
+        errorMessage: "Por favor rellena todos los campos.",
+      });
+      return;
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.render("auth/login", {
+        errorMessage: "Usuario o contraseña incorrectos.",
+      });
+      return;
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      res.render("auth/login", {
+        errorMessage: "Usuario o contraseña incorrectos.",
+      });
+      return;
+    }
+
+    req.session.currentUser = user;
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/profile", isLoggedIn, (req, res, next) => {
+  res.render("auth/profile");
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(() => res.redirect("/"));
+});
 
 module.exports = router;
