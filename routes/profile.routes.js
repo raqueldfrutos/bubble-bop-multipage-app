9d773d76812284d4c4789f3982b2dc4207ef66f1
raqueldfrutos <raqueldfrutos@gmail.com>
2023-05-07 19:11:22 +0200

    update

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./../models/User.model");
-const {isLoggedIn}  = require("../middlewares/route-guard");
-
+const { isLoggedIn } = require("../middlewares/route-guard");
 
 router.get("/", isLoggedIn, (req, res, next) => {
-  const { currentUser } = req.session
-    res.render("profile", { currentUser });
+  const { currentUser } = req.session;
+  User.findById(currentUser._id).then(user => {
+    console.log(user);
+    res.render("profile/profile", user);
   });
+});
+
+router.get("/edit", isLoggedIn, (req, res, next) => {
+  const { currentUser } = req.session;
+  User.findById(currentUser._id).then(user => {
+    res.render("profile/edit-profile", user);
+  });
+});
+
+router.post("/edit", isLoggedIn, (req, res, next) => {
+  const { currentUser } = req.session;
+  User.findByIdAndUpdate(currentUser._id, req.body).then(() => {
+    res.redirect("/profile");
+  });
+});
 
 module.exports = router;
