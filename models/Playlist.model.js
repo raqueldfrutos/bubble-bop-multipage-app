9e78d22945f680fb5f8f2824170035e303026a4a
raqueldfrutos <raqueldfrutos@gmail.com>
2023-05-11 21:39:21 +0200

    playlist crud: create, push and show in user profile

--- models/Playlist.model.js
+++ models/Playlist.model.js
@@ -4,9 +4,12 @@ const playlistSchema = new Schema(
   {
     name: {
       type: String,
-      required: true
+      required: true,
     },
-    playlistImage: String,
+    playlistImage: {
+      type: String,
+      default: "https://yt3.googleusercontent.com/ytc/AGIKgqPmFbh4WiZlfw9spG8Dtoy8zY616IV9tAB7A9oGyg=s900-c-k-c0x00ffffff-no-rj"
+    },
     description: String,
     tracks: [String]
   },
