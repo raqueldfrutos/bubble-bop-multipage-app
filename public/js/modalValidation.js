ca11402999fbd4771dbab29e4550a3bcffc7d576
raqueldfrutos <raqueldfrutos@gmail.com>
2023-05-13 12:46:48 +0200

    create playlist modal ok

--- public/js/modalValidation.js
+++ public/js/modalValidation.js
@@ -1,13 +1,10 @@
 const modalButton = document.getElementById("modal-button");
-const titleInput = document.getElementById("playlist-title").value;
+const titleInput = document.getElementById("playlist-title");
 
-window.onload = () => {
-  console.log("hola");
-};
-titleInput.onchange = () => {
-  console.log(titleInput);
-
-  if (titleInput) {
+titleInput.onkeyup = () => { // comprueba cada vez que tecleamos, si el imput se completa, el botón de create se activa
+  if (titleInput.value !== "") {
     modalButton.disabled = false;
+  } else {
+    modalButton.disabled = true;
   }
 };
